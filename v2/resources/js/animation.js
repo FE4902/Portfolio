a55154bb337d2e6c63592682bd1bc993e2983057
FE4902 <isdn000123@gmail.com>
2023-03-22 23:27:26 +0900

    Feat: 버전 2 Project 추가

--- v2/resources/js/animation.js
+++ v2/resources/js/animation.js
@@ -10,3 +10,22 @@ gsap.from(".company__swipe--after", {
     clipPath: "inset(100% 0px 0px)",
     ease: "none",
 });
+
+const elements = document.querySelectorAll(".project__link");
+elements.forEach((element) => {
+    const left = (Math.random() - 0.5) * window.innerWidth;
+    const top = Math.random() * -(window.innerHeight - element.offsetHeight);
+
+    gsap.from(element, {
+        scrollTrigger: {
+            trigger: ".project",
+            scrub: 1,
+            start: "0",
+            end: "0",
+        },
+        left: left,
+        top: top,
+        opacity: 0,
+        ease: "none",
+    });
+});
