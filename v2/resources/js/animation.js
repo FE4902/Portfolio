900ea2e01e27411c96926ec12a9ed1de7dc1fafd
FE4902 <isdn000123@gmail.com>
2023-03-25 13:09:38 +0900

    Feat: 버전 2 Intro 추가

--- v2/resources/js/animation.js
+++ v2/resources/js/animation.js
@@ -1,5 +1,59 @@
 gsap.registerPlugin(ScrollTrigger);
 
+// INTRO
+const animationOptions = {
+    ease: "expo.inOut",
+};
+
+const introAnimation = () => {
+    const tl = gsap.timeline({
+        defaults: {
+            ease: animationOptions.ease,
+            duration: 1,
+        },
+    });
+
+    tl.to(".intro__title", {
+        duration: 1.5,
+        y: 0,
+        autoAlpha: 1,
+        delay: 0.5,
+    })
+        .to(".intro__background--left, .intro__background--right", {
+            scaleX: 1,
+        })
+        .to(".intro__background--left, .intro__background--right", {
+            scaleY: 0,
+            transformOrigin: "top center",
+        })
+        .to(
+            ".intro__title",
+            {
+                duration: 1.5,
+                y: -60,
+                autoAlpha: 0,
+            },
+            "-=0.6"
+        )
+        .to(
+            ".intro",
+            {
+                y: "-100%",
+            },
+            "-=0.5"
+        );
+
+    return tl;
+};
+
+const master = gsap.timeline({
+    paused: false,
+    delay: 0.2,
+});
+
+master.add(introAnimation());
+
+// COMPANY
 gsap.from(".company__swipe--after", {
     scrollTrigger: {
         trigger: ".company",
@@ -11,6 +65,7 @@ gsap.from(".company__swipe--after", {
     ease: "none",
 });
 
+// PROJECT
 const elements = document.querySelectorAll(".project__link");
 elements.forEach((element) => {
     const left = (Math.random() - 0.5) * window.innerWidth;
@@ -30,6 +85,7 @@ elements.forEach((element) => {
     });
 });
 
+// SCROLL
 gsap.to(".scroll__line--top", {
     scrollTrigger: {
         trigger: ".project",
