9f56481a3937749f354cf3f93aed8f4bf667ce3e
FE4902 <isdn000123@gmail.com>
2023-09-13 22:43:10 +0900

    Reset

--- v1/resources/js/gotop.js
+++
@@ -1,9 +0,0 @@
-let goToTopButton = document.querySelector(".go-to-top-button");
-
-window.addEventListener("scroll", () => {
-    window.scrollY === 0 ? goToTopButton.classList.remove("-active") : goToTopButton.classList.add("-active");
-});
-
-goToTopButton.addEventListener("click", (e) => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-});
