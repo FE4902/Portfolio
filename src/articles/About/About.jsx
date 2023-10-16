fd480dfd7396a6d71ca6a7bc7a98f6a82b550d4f
FE4902 <isdn000123@gmail.com>
2023-10-16 23:35:04 +0900

    Feat: 버전 3 About

--- src/articles/About/About.jsx
+++ src/articles/About/About.jsx
@@ -5,7 +5,19 @@ import * as styles from "./About.module.scss";
 const c = classNames.bind(styles);
 
 const About = () => {
-    return <article className={c("about")}></article>;
+    return (
+        <article className={c("about")}>
+            <div className={c("container")}>
+                <h2 className={c("title")}>ABOUT</h2>
+                <p className={c("desc")}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Autem animi minus totam eligendi in facilis quam blanditiis
+                    enim nesciunt porro. Minima dicta facere accusantium
+                    quaerat. Praesentium at modi facere architecto!
+                </p>
+            </div>
+        </article>
+    );
 };
 
 export default About;
