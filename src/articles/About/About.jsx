eff365e61c2061c310e6f53dac1c94bcdfb641f9
FE4902 <isdn000123@gmail.com>
2023-10-25 22:31:03 +0900

    Refactor: 버전 3 About 수정

--- src/articles/About/About.jsx
+++ src/articles/About/About.jsx
@@ -1,20 +1,88 @@
 import classNames from "classnames/bind";
+import { motion } from "framer-motion";
 
 import * as styles from "./About.module.scss";
+import Bracket from "../../components/Bracket/Bracket";
 
 const c = classNames.bind(styles);
 
 const About = () => {
+    const container = {
+        inactive: {},
+        active: {
+            transition: {
+                delayChildren: 0.6,
+                duration: 0.05,
+                staggerChildren: 0.05,
+            },
+        },
+    };
+
+    const textColor = {
+        inactive: {
+            color: "rgba(255, 255, 255, 1)",
+        },
+        active: {
+            color: [
+                "rgba(255, 255, 255, 1)",
+                "rgba(254, 73, 2, 1)",
+                "rgba(254, 73, 2, 1)",
+                "rgba(255, 255, 255, 1)",
+            ],
+        },
+    };
+
+    const textY = {
+        inactive: {
+            opacity: 0,
+            color: "rgba(255, 255, 255, 1)",
+            y: 30,
+        },
+        active: {
+            opacity: [0, 1, 1, 1],
+            color: [
+                "rgba(255, 255, 255, 1)",
+                "rgba(254, 73, 2, 1)",
+                "rgba(254, 73, 2, 1)",
+                "rgba(255, 255, 255, 1)",
+            ],
+            y: [30, 0, 0, 0],
+        },
+    };
+
     return (
         <article className={c("about")}>
             <div className={c("container")}>
-                <h2 className={c("title")}>ABOUT</h2>
-                <p className={c("desc")}>
+                <Bracket>ABOUT</Bracket>
+                <motion.div
+                    className={c("desc")}
+                    variants={container}
+                    initial="inactive"
+                    whileInView={["visible", "active"]}
+                >
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Autem animi minus totam eligendi in facilis quam blanditiis
+                    Autem animi minus totam eligendi in
+                    {[..."facilis quam blanditiis"].map((v, i) => {
+                        return (
+                            <motion.span key={i} variants={textColor}>
+                                {v}
+                            </motion.span>
+                        );
+                    })}
                     enim nesciunt porro. Minima dicta facere accusantium
-                    quaerat. Praesentium at modi facere architecto!
-                </p>
+                    quaerat. Praesentium at modi facere architecto! Lorem, ipsum
+                    dolor sit amet consectetur adipisicing elit. Sapiente,
+                    asperiores aliquam! Labore possimus exercitationem omnis sed
+                    neque reprehenderit accusamus dignissimos, ex officia
+                    repudiandae alias culpa, autem,
+                    {[..."commodi debitis illo voluptatibus!"].map((v, i) => {
+                        return (
+                            <motion.span key={i} variants={textY}>
+                                {v}
+                            </motion.span>
+                        );
+                    })}
+                </motion.div>
             </div>
         </article>
     );
