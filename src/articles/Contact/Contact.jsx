4aa6845218a20a6346db10036bf74e83f69ac387
FE4902 <isdn000123@gmail.com>
2023-10-28 23:08:11 +0900

    Feat: 버전 3 Contact

--- src/articles/Contact/Contact.jsx
+++ src/articles/Contact/Contact.jsx
@@ -1,12 +1,14 @@
 import classNames from "classnames/bind";
 
 import * as styles from "./Contact.module.scss";
+import Background from "../../components/Background/Background";
 
 const c = classNames.bind(styles);
 
 const Contact = () => {
     return (
         <article id="contact" className={c("contact")}>
+            <Background />
             <h2>CONTACT</h2>
             <p>안주홍</p>
             <p>WEB PUBLISHER</p>
